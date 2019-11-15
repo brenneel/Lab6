ca704cc96da5dde6577a6b7e7e9acb8935b60885
Ethan <ethanlbrenner@gmail.com>
2019-11-15 14:26:55 -0600

    created Test.h and Test.cpp

---
+++ Test.h
@@ -0,0 +1,24 @@
+/**
+ * Author: Ethan Brenner
+ * Desc: Class to test the functionality of Queue
+*/
+
+#ifndef TEST_H
+#define TEST_H
+
+#include "Queue.h"
+#include <stdexcept>
+
+class Test{
+
+    private:
+    //private members
+
+    public:
+    //public members
+
+
+
+
+};
+#endif
